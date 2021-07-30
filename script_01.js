75fcaf3b9a2168150b6916fc3f718c9037353ab3
Peter <schmidt.multimedia@gmail.com>
2021-07-30 09:30:44 +0200

    01 / Fkt. als Kapselung von Code + calls()

--- script_01.js
+++ script_01.js
@@ -1,2 +1,14 @@
 /***** Funktionen 01 *****/
 
+// 1. Kapselung von Codeblöcken
+
+// Funktionsrumpf (body) | callee
+function test()
+{
+    console.log("Hallo Kevin!");
+}
+
+// Funktionsaufruf (call)
+test();
+
+
