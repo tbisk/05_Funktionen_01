1e9ac35c758f7d90ff06de4b4c4bb9dfc8b1c898
Peter <schmidt.multimedia@gmail.com>
2021-07-30 10:46:05 +0200

    02a / Parametrisierung / lokale Variable

--- script_01.js
+++ script_01.js
@@ -2,13 +2,24 @@
 
 // 1. Kapselung von Codeblöcken
 
+// Funktionsaufruf (call)
+// test();
+
 // Funktionsrumpf (body) | callee
 function test()
 {
     console.log("Hallo Kevin!");
 }
 
-// Funktionsaufruf (call)
-test();
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
 
+function ausgabeNamen() 
+{
+    const firstName = "Kevin"; // Variable --> lokal scope
+    console.log("Hallo " + firstName + "!");
+}
+
+// ausgabeNamen();
+// console.log(firstName);  // Fehler --> lokal!!!
 
